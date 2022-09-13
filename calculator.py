660f03db2301bfa95220be63e1f08e7702d92c66
Mohamad <mfmohamadfaez@gmail.com>
2022-09-13 23:05:37 +0800

    update calculator

--- calculator.py
+++ calculator.py
@@ -17,3 +17,30 @@ def multiply(first_term, second_term):
 
 def divide(first_term, second_term):
     return first_term / second_term
+
+
+if __name__ == "__main__":
+    # choose arithmetic operations
+    print("""
+    Please choose an arithmetic operation from options below:
+    a. Addition
+    b. Subtraction
+    c. Multiplication
+    d. Division
+    """)
+    option = input("Enter your option: ")
+    if option in 'abcd':
+        num1, num2 = input("Enter two values: ").split()
+        # sanitise numbers
+        num1 = int(num1)
+        num2 = int(num2)
+        if option == 'a':
+            print("result:", add(num1, num2))
+        elif option == 'b':
+            print("result:", subtract(num1, num2))
+        elif option == 'c':
+            print("result:", multiply(num1, num2))
+        else:
+            print("result:", divide(num1, num2))
+    else:
+        print('invalid option')
